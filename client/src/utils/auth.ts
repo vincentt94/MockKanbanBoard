0838c15c8b54d31cb9b5101b9d7a7a00a8006f61
vincentt94 <vincent.h.thao@gmail.com>
2025-02-13 09:15:13 -0600

    Add files via upload

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,47 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    const decoded = jwtDecode<JwtPayload>(token);
+    return decoded;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
-  isTokenExpired(token: string) {
+  isTokenExpired() {
     // TODO: return a value that indicates if the token is expired
+    const data =  this.getProfile();
+    const expInMilli = (data?.exp ? data.exp: 0) * 1000;
+    if(expInMilli < Date.now()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/')
   }
 }
 
